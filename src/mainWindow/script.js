32b72d038d7d360528b6f9965b94f0a4305708bc
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-07-17 15:33:19 -0300

    tagging elements id/class, começando a fazer o icone quadrado de stop

---
+++ src/mainWindow/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const container = document.getElementById('container');
+    container.querySelector('#record').classList.add('recording');
+    container.querySelector('#mic-icon').classList.add('hide');
+
+    
+})
